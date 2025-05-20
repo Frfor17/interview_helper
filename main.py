cc28b769dcd2e73f011af8380213a9b9e4aa5a84
Фёдор Хлевицкий <brother.23@yandex.ru>
2025-05-20 14:14:29 +0300

    shit
    ну да

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 import os
+from dotenv import load_dotenv
+import openai 
+
 
 app = FastAPI()
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
